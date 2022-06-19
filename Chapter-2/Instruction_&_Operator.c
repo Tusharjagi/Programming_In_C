2b6cfe3f7576549ca8580d500f023a222e8c7455
Tushar jagi <tusharjagi@gmail.com>
2022-06-19 14:44:01 +0530

    Instruction

---
+++ Chapter-2/Instruction_&_Operator.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    int a = 22;
+    int b = a;
+    int c = b + c;
+    int d = 1, e;
+
+    int old_Age = 22;
+    int year_Age;
+    int new_Age = old_Age + year_Age;
+
+    
+
+
+    return 0;
+}
