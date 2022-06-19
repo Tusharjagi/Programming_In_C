a580866ce3f61026b6a4b4ccc30bd2c245d44c6f
Tushar jagi <tusharjagi@gmail.com>
2022-06-19 14:01:40 +0530

    Variables

--- Chapter-1/Variable.c
+++ Chapter-1/Variable.c
@@ -3,9 +3,16 @@
 int main(){
 
     int number = 25;
-    char ch = 'c';
-    int age = 22;
     float pi = 3.14;    
+    char ch = 'c';
 
+    char start = '*';
+    printf("star is %c \n",start);
+
+
+    int age;
+    printf("Enter your age :\t");
+    scanf("%d",&age);
+    printf("Age is :: %d",age);
     return 0;
 }
