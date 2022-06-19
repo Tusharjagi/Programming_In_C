69115106f38b730315b83804de6c3240a5662e21
Tushar jagi <tusharjagi@gmail.com>
2022-06-19 16:51:03 +0530

    student

--- Practice/Student_Pass.c
+++ Practice/Student_Pass.c
@@ -5,11 +5,14 @@ int main(){
     printf("Enter the student marks : \t");
     scanf("%d",&m);
 
-    if(m > 30){
-        printf("Pass");
+    if(m >= 0 && m <= 30){
+        printf("Fail");
+    }
+    else if(m >= 30 && m <= 100){
+        printf("pass");
     }
     else{
-        printf("Fail");
+        printf("Wrong marks");
     }
 
     return 0;
