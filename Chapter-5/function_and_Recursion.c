8224e781925c754175e6e95d85e47e500fb1d4df
Tushar jagi <tusharjagi@gmail.com>
2022-06-20 08:54:54 +0530

    function

---
+++ Chapter-5/function_and_Recursion.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+// void printHello(){
+//     printf("Hello !\n");
+//     printf("Tushar ");
+// }
+
+void Hello(){
+    printf("Hello \n");
+}
+
+void GB(){
+    printf("Good Byee");
+}
+
+int main(){
+
+    Hello();
+    GB();
+
+    return 0;
+}
+
