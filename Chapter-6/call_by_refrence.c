dc3fa554ac8d9dac53ef32a8bdd1138fe083aab5
Tushar jagi <tusharjagi@gmail.com>
2022-06-21 13:16:55 +0530

    call by refrence

--- Chapter-6/call_by_refrence.c
+++ Chapter-6/call_by_refrence.c
@@ -1,8 +1,16 @@
 #include<stdio.h>
 
+void Square(int* n){
+    *n  = *n * *n;
+    printf("Square = %d \n",*n);
+}
+
 int main(){
     
-
+    int number = 4;
+    Square(&number);
+    printf("number = %d \n", number);
+    
 
     return 0;
 }
