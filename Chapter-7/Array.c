df53f118e5b4274a646c94f0a89c2ee6a463b6d6
Tushar jagi <tusharjagi@gmail.com>
2022-06-23 13:03:19 +0530

    array

--- Chapter-7/Array.c
+++ Chapter-7/Array.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 
 int main(){
+
+    int arr[] = {12,435,24,3452,3432,252};
+    int n = 10;
+    for(int i = 0; i < n; i++ ){
+        printf("%d  ",arr );
+    }
     
     return 0;
 }
