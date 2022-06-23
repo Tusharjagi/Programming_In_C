82232211a1421e544f4c426db521e05dc4b8daeb
Tushar jagi <tusharjagi@gmail.com>
2022-06-23 13:34:49 +0530

    string

--- Practice/string_multi.c
+++ Practice/string_multi.c
@@ -2,10 +2,20 @@
 
 int main(){
 
-    char name[100];
-    // gets(name);
-    fgets(name,100,stdin);
-    puts(name);
+    // char name[100];
+    // // gets(name);
+    // fgets(name,100,stdin);
+    // puts(name);
+
+    char *change = "Hello world ";
+    puts(change);
+    change = "Hello";
+    puts(change);
+
+    // char cannotChange[] = "hello world";
+    // puts(cannotChange);
+    // cannotChange = "hello";
+
 
     return 0;
 }
