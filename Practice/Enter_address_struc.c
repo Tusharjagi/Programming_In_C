69064be2ad03d2e07e37399592e2941c1afef2cc
Tushar jagi <tusharjagi@gmail.com>
2022-06-23 14:49:10 +0530

    Enter address

---
+++ Practice/Enter_address_struc.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+typedef struct address{
+
+    int house;
+    int block;
+    char city[100];
+    char state[100];
+
+}add;
+
+int main(){
+    
+    add p1;
+    p1.block;
+    p1.city;
+    p1.house;
+    p1.state;
+
+
+    return 0;
+}
