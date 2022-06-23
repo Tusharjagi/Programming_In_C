102a50a01108dd82b22016f3f46c59eecf3bf71e
Tushar jagi <tusharjagi@gmail.com>
2022-06-23 14:44:09 +0530

    Type_def

---
+++ Chapter-9/type_def.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+typedef struct ComputerEngineerStudent{
+    int roll;
+    float cgpa;
+    char name[100];
+}coe;
+
+int main(){
+   coe s1;
+   s1.cgpa = 34;
+//    s1.name = "dhfsd";
+   s1.roll = 4243;
+    
+    
+
+    return 0;
+}
